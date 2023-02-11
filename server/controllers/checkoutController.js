4bb2095eb1599cdfe750b9faf711fcde3932a805
Christian Smith <christian.46.smith@gmail.com>
2023-02-10 22:37:38 -0500

    create functional order amount calculator that uses stripe api to get prices

--- server/controllers/checkoutController.js
+++ server/controllers/checkoutController.js
@@ -2,14 +2,29 @@ require('dotenv').config();
 const stripeKey = process.env.STRIPE_KEY;
 const stripe = require("stripe")(stripeKey);
 
-const calculateOrderAmount = (items) => {
-    // Just creating a temp function to calculate price while testing
-    return 100;
+const calculateOrderAmount = async (items) => {
+    let total;
+    // For each item retrieve price and multiply by item count
+    for(let item of items){
+        // Use stripe api and extend to get the price based on product id
+        const product = await stripe.products.retrieve(item.id, {expand: ['default_price']});
+        if(total){
+            total += product.default_price.unit_amount * item.count;
+        } else {
+            total = product.default_price.unit_amount * item.count;
+        }
+    }
+    if(total){
+        return total;
+    } else {
+        return -1;
+    }
 }
 
 module.exports = {
     async createPaymentIntent(req, res) {
         const { items } = req.body;
+        console.log({items});
         // Alternatively, set up a webhook to listen for the payment_intent.succeeded event
         // and attach the PaymentMethod to a new Customer
         const customer = await stripe.customers.create();
@@ -18,7 +33,7 @@ module.exports = {
         const paymentIntent = await stripe.paymentIntents.create({
             customer: customer.id,
             setup_future_usage: "off_session",
-            amount: calculateOrderAmount(items),
+            amount: await calculateOrderAmount(items),
             currency: "usd",
             automatic_payment_methods: {
                 enabled: true,
