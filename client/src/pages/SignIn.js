3918a54163869739e3d0ea57b9835de02dfba1a6
Christian Smith <christian.46.smith@gmail.com>
2023-03-21 23:35:11 -0400

    add state to signin page

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function SignIn() {
     const navigate = useNavigate();
+    const [password, setPassword]= useState('');
+    const [email, setEmail] = useState('');
 
     return (
         <>
             <h1>Sign In</h1>
             <form>
                 <label>Email Address</label>
-                <input type={'email'} placeholder='example@email.com'></input>
+                <input type={'email'} placeholder='example@email.com' value={email} onChange={(event) => setEmail(event.target.value)}></input>
                 <label>Password</label>
-                <input type={'password'} placeholder='password'></input>
+                <input type={'password'} placeholder='password' value={password} onChange={(event) => setPassword(event.target.value)}></input>
                 <button type={'submit'}>Sign In</button>
             </form>
             <button onClick={() => navigate('/create-account')}>Create Account</button>
